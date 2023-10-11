e0a6aff7e18e5a714fea265a254ecc1a7419dae7
DidierVer <94317582+DidierVerNL@users.noreply.github.com>
2023-10-11 16:23:06 +0200

    Finished Menu Mock-up

--- ArcadeLauncher/ArcadeLauncher/Models/Game.cs
+++ ArcadeLauncher/ArcadeLauncher/Models/Game.cs
@@ -6,7 +6,6 @@
         public string Description { get; set; }
         public string[] Authors { get; set; }
         public string NameExe { get; set; }
-        public string NameFolder {  get; set; }
         public string CompleteFolder;
     }
 }
--- ArcadeLauncher/ArcadeLauncher/Services/GameService.cs
+++ ArcadeLauncher/ArcadeLauncher/Services/GameService.cs
@@ -1,17 +1,15 @@
-﻿using System.Diagnostics;
-using ArcadeLauncher.Models;
+﻿using ArcadeLauncher.Models;
 using Newtonsoft.Json;
+using System.Diagnostics;
 namespace ArcadeLauncher.Services
 {
     public class GameService
     {
         public IWebHostEnvironment env;
-        public GameService(IWebHostEnvironment environment) 
-        { 
+        public GameService(IWebHostEnvironment environment)
+        {
             env = environment;
         }
-
-        
 
         public string[] GetAllFolders()
         {
@@ -21,7 +19,6 @@ namespace ArcadeLauncher.Services
         }
         public Game[] GetAllManifests()
         {
-
             var folders = GetAllFolders();
             List<Game> manifests = new List<Game>();
 
@@ -34,15 +31,22 @@ namespace ArcadeLauncher.Services
                     game.CompleteFolder = folders[i];
                     manifests.Add(game);
                 }
-               
             }
-
             return manifests.ToArray();
         }
 
         public void OpenExe(Game executable)
         {
-            Process.Start(Path.Combine(executable.CompleteFolder, executable.NameExe + ".exe"));
+            Process[] processList = Process.GetProcessesByName(executable.NameExe);
+            if (processList.Length == 0)
+                Process.Start(Path.Combine(executable.CompleteFolder, executable.NameExe + ".exe"));
+        }
+
+        public string ShowImage(Game gameFolder)
+        {
+            byte[] imageArray = System.IO.File.ReadAllBytes(Path.Combine(gameFolder.CompleteFolder, "icon.png"));
+            string base64Image = Convert.ToBase64String(imageArray);
+            return base64Image;
         }
     }
 }
