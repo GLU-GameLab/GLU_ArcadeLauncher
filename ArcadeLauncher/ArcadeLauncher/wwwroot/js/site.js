e30099d29f78feda6e73f745be7500aec755fa4a
DidierVer <94317582+DidierVerNL@users.noreply.github.com>
2023-11-02 13:30:29 +0100

    Added the main background and a scroll

--- ArcadeLauncher/ArcadeLauncher/wwwroot/js/site.js
+++ ArcadeLauncher/ArcadeLauncher/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 900;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+
+
+function scroller() {
+    window.scroll({top: 1130, behavior: "smooth",})
+}
+
+window.addEventListener("scroll", reveal);
