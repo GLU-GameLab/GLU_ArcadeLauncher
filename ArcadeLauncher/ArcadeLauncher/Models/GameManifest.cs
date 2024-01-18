9f5f4240c746cf71d38e04e1f1b3dd7b9e16bba3
zjustan <46072158+zjustan@users.noreply.github.com>
2024-01-18 16:40:13 +0100

    more sturff

--- ArcadeLauncher/ArcadeLauncher/Models/GameManifest.cs
+++ ArcadeLauncher/ArcadeLauncher/Models/GameManifest.cs
@@ -6,9 +6,9 @@
         public string Description { get; set; }
         public string[] Authors { get; set; }
         public string NameExe { get; set; }
-        public string CompleteFolder;
         public string BackgroundColor { get; set; }
 
         public int PlayersNeeded { get; set; }
+        public int ManifestVersion { get; set; } = 1;
     }
 }
